a706a4203d935e4af6f495a65507520c4834cc6a
RiceDanAwesom12 <riced8@mail.uc.edu>
2025-08-08 11:08:09 -0400

    Add files via upload

---
+++ EFCoreWebApi/Models/FavoritePlanet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Group7_Final_Project.EFCoreWebApi.Models
+{
+    public class FavoritePlanet
+    {
+        public int Id { get; set; }
+        public string PlanetName { get; set; } = string.Empty;
+        public string Allegiance { get; set; } = string.Empty;
+        public DateTime AllegianceDate { get; set; }
+        public string Species { get; set; } = string.Empty;
+        public string PlanetarySystem { get; set; } = string.Empty;
+        public string Geography { get; set; } = string.Empty;
+    }
+}
